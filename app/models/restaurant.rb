2f554c4b0b1ef044ea943fc8ce58dea5188bbf8d
github username <github email address>
2018-07-27 11:34:01 -0500

    TEST mod 1

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,42 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |restaurant|
+      if restaurant.name == name
+        restaurant
+      end
+    end
+  end
+
+  def customers
+    self.reviews.map do |review|
+      review.customer
+    end.uniq
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def average_star_rating #array of ratings, then average
+    self.reviews.map do |review|
+      review.rating
+    end
+    #still need to average...
   end
 
 end
