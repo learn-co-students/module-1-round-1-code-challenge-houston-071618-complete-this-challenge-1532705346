fa6e5449a3a5d853c40cc637f551ddaca3493d02
pandasandeggs <theteatreehouse@gmail.com>
2018-07-27 11:32:51 -0500

    code challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,34 @@
 class Restaurant
-  attr_accessor :name
+  @@all = []
+  attr_accessor :name, :rating
 
-  def initialize(name)
+  def initialize(name,rating)
     @name = name
+    @rating = rating
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customers
+    reviews.map do |review|
+      review.customer
+    end
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |restaurant|
+      restaurant.name == name
+    end
+    name
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,21 @@
 class Review
-  
-end
+  @@all = []
+  attr_reader :customer, :restaurant
 
+  def initialize(customer,restaurant)
+    @customer = customer
+    @restaurant = restaurant
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  # def rating(restaurant)
+  #   Restaurant.all.select do |restaurant|
+  #     restaurant.rating
+  #   end
+  # end
+
+end
