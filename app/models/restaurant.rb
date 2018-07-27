66cc0e23c047a9ee5865d920cce200d0fd5cdd07
yendai-vo <yendai.vo@hotmail.com>
2018-07-27 11:32:32 -0500

    finished most of the challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,45 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |r|
+      r.name == name
+    end
+  end
+
+  #didn't finish!
+  
+  # def customers
+  #   Customer.all.select do |customer|
+  #     customer.
+ 
+  # all reviews that belong to a restaurant
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    count = reviews.length
+    sum = reviews.map(&:rating).reduce(:+)
+    sum / count
+  end
+  
+  # use the inst. method reviews
+  # return array of contents
+  # pick out the longest review
+  def longest_review
+    reviews.map(&:content).max_by(&:length)
+  end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,16 @@
 class Review
-  
-end
+  attr_reader :customer, :restaurant
+  @@all = []
 
+  def initialize(customer, restaurant, content = "", rating = [1..5])
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
