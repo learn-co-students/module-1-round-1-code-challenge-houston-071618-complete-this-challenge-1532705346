c39c0fd0e6db30844966a5cc6e1797cff6528b3f
Alejandro Aguilar Jr <alejandro.aguilar.jr@gmail.com>
2018-07-27 11:20:18 -0500

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,56 @@
 class Customer
+  @@all =[]
   attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def add_review(restaurant,content,rating)
+    Review.new(self,restaurant,rating,content)
+  end
+
+  def num_reviews
+    (Review.all.select do |review|
+      review.customer == self
+    end).length
+  end
+
+  def restaurants
+      my_reviews=Review.all.select do |review|
+        review.customer == self
+      end
+
+      (my_reviews.map do |review|
+        review.restaurant
+      end).uniq
+  end
+
+  def self.find_by_name(full_name)
+    @@all.find do |customer|
+      customer.full_name.downcase == full_name
+    end
+  end
+
+  def self.find_all_by_first_name(first_name)
+    @@all.select do |customer|
+      customer.first_name.downcase == first_name.downcase
+    end
+  end
+
+  def self.all_names
+    @@all.map do |customer|
+      customer.first_name.concat(" #{customer.last_name}")
+    end
+  end
+
+  def self.all
+    @@all
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,8 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/customer.rb'
+require_relative '../app/models/restaurant.rb'
+require_relative '../app/models/review.rb'
+
 
 def reload
   load 'config/environment.rb'
@@ -6,6 +10,18 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+
+alex = Customer.new("alex","aguilar")
+belle = Customer.new("belle","carrillo")
+karen = Customer.new("karen","soriano")
+
+dominos = Restaurant.new("Dominos")
+thai = Restaurant.new("thai kitchen")
+
+alex_rates_domino = Review.new(alex, dominos, 2, "This place was just ok." )
+alex_rates_domino2 = Review.new(alex, dominos, 5, "This place was just ok." )
+karen_rates_thai = Review.new(karen, thai, 5, "This place was great." )
+karen_rates_domino3 = Review.new(karen, dominos, 1, "This place was the worst. I could go on but this is good enough for testing" )
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
