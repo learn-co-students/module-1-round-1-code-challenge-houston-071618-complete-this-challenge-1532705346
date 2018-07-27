d0f066e6f0e9a80e5facc3901f4413593553b0e0
Christopher Arnold <chrisarnold2010@gmail.com>
2018-07-27 11:32:05 -0500

    Done-zo?

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,57 @@
 class Customer
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :review, :content
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
-    "#{first_name} #{last_name}"
+    "#{@first_name} #{@last_name}"
   end
+
+  def self.find_by_name(full_name)
+  	self.all.find do |customer|
+  		customer.full_name = full_name
+  	end
+  end
+
+  def self.find_all_by_first_name(first_name)
+  	self.all.select do |customer|
+  		customer.first_name = first_name
+  	end
+  end
+
+  def add_review(restaurant,review,content)
+  	Review.new(self,restaurant,review,content)
+  end
+
+  # def self.all_names
+
+  # end
+
+  def self.all
+  	@@all
+  end
+
+  # def restaurants
+  # 	Review.all.select do |review|
+  # 		review.customer == self
+  # 	end
+  # end
+
 end
+
+
+
+
+
+# - `Customer.all_names`
+#   - should return an **array** of all of the customer full names
+# - `Customer#num_reviews`
+#   - Returns the total number of reviews that a customer has authored
+# - `Customer#restaurants`
+#   - Returns a **unique** array of all restaurants a customer has reviewed
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review
+
+	attr_reader :customer, :restaurant
+	attr_accessor :review, :content
+
+	@@all =[]
+
+	def initialize(customer,restaurant,review,content)
+		@customer = customer
+		@restaurant = restaurant
+		@review = review
+		@content = content
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
   
 end
+
 
