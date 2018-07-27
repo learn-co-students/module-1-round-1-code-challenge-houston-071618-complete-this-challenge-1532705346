820368058a28b2f6193e67c380b20bdc34f1b60d
Flatiron School <flatironschool@Flatirons-MacBook-Air-3.local>
2018-07-27 11:32:05 -0500

    MODULE 1 code

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
-class Review
-  
-end
+require 'pry'
+require_relative './restaurant.rb'
+require_relative './customer.rb'
 
+class Review
+  attr_reader :customer, :restaurant
+  attr_accessor :rating, :content # we want to be able to change these.
+  @@all = []
+  def initialize(customer, restaurant, rating, content)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+end
