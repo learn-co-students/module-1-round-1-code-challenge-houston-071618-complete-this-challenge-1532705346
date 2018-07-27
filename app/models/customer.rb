601a03b5f6c917413859ad1294c2a96d2f9882dd
Ray <ray.rodz.88@gmail.com>
2018-07-27 11:31:51 -0500

    challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,65 @@
 class Customer
   attr_accessor :first_name, :last_name
 
+  @@all = []
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    Customer.all.find do |customer|
+      customer.full_name == name
+      # binding.pry
+    end
+  end
+
+  def self.find_all_by_first_name(name)
+    Customer.all.map do |customer|
+      if customer.first_name == name
+        # binding.pry
+      end
+    end
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end
+
+  def num_reviews
+    Review.all.map do |all_reviews|
+      all_reviews.customer == self
+      #array of instances
+    end
+  end
+
+    def restaurants
+      reviews_by = []
+      Review.all.each do |reviews|
+        if reviews.customer == self
+          reviews_by << reviews
+        end
+      end
+        return reviews_by
+    end
+
+
+
+
+
 end
+#
+# - `Customer#num_reviews`
+#   - Returns the total number of reviews that a customer has authored
+# - `Customer#restaurants`
+#   - Returns a **unique** array of all restaurants a customer has reviewed
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,36 @@
 class Review
+
+  attr_reader :customer, :restaurant
+
+  @@all = []
+
+  def initialize(customer, restaurant)
+    @customer = customer
+    @restaurant = restaurant
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
   
+
+
 end
 
+
+#### Build out the following methods on the `Review` class
+
+# - `Review.all`
+#   - returns all of the reviews
+# - `Review#customer`
+#   - returns the customer object for that given review
+#   - Once a review is created, I should not be able to change the author
+# - `Review#restaurant`
+#   - returns the restaurant object for that given review
+#   - Once a review is created, I should not be able to change the restaurant
+# - `Review#rating`
+#   - returns the star rating for a restaurant. This should be an integer from 1-5
+# - `Review#content`
+#   - returns the review content, as a string, for a particular review
