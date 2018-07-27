7fb3a8b9bfe1cfc2a4a808618939b3e071b7e0d9
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-27 11:32:26 -0500

    Code Challenge

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,58 @@
 class Customer
   attr_accessor :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @full_name = self.full_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
-end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find { |customer| customer.full_name == name}
+  end
+
+  def self.find_all_by_first_name(name)
+    #given a string of a first name, returns an
+    #**array** containing all customers with that
+    #first name
+    @@all.select {|customer| customer.first_name == name}
+  end
+
+  def self.all_names
+    #should return an **array**
+    #of all of the customer full names
+    @@all.map {|customer| customer.full_name}
+  end
+
+  def add_review(restaurant, content, rating)
+    new_review = Review.new(self, restaurant,content,rating)
+    new_review.customer = self
+  end
+
+  def num_reviews
+    r_array = Review.all.select {|review| review.customer == self}
+    r_array.length
+  end
+
+  def restaurants
+    r_array = Review.all.select {|review| review.customer == self}
+    r_array.map {|review| review.restaurant}
+    r_array.uniq!
+  end
+
+
+
+
+
+
+end #class
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,21 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+customer1 = Customer.new("Jane", "Doe")
+customer2 = Customer.new("Mary","Poppins")
+customer3 = Customer.new("Mad", "Max")
+
+restaurant1 = Restaurant.new("Fuddruckers")
+restaurant2 = Restaurant.new("Starr Pizza")
+restaurant3 = Restaurant.new("Cheesecake Factory")
+
+value = Customer.find_by_name("Jane Doe")
+find_by_first_name = Customer.find_all_by_first_name("Mad")
+find_all_names = Customer.all_names
+
+find_restaurant = Restaurant.find_by_name("Starr Pizza")
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
