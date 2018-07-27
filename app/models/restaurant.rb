567d07d4d68a622afa97021f1fc3172167984a4a
Kenny Yang <kennyyang19@gmail.com>
2018-07-27 11:31:57 -0500

    completed

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,51 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    @@all.find do |restaurant|
+      restaurant.name == name
+    end
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    reviews.map do |review|
+      review.customer
+    end
+  end
+
+  def average_star_rating
+    average_rating = reviews.map do |review|
+      review.rating
+    end
+    average_rating.reduce(:+)/(average_rating.length).to_f
+  end
+
+  def longest_review
+    x = reviews.map do |review|
+      review.content
+    end
+    x.max_by.size
   end
 
 end
+
+# - `Restaurant#average_star_rating`
+#   - returns the average star rating for a restaurant based on its reviews
+# - `Restaurant#longest_review`
+#   - returns the longest review content for a given restaurant
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,49 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+
+burger_king = Restaurant.new("Burger King")
+wendys = Restaurant.new("Wendys")
+pho = Restaurant.new("PhoSo1")
+
+kenny = Customer.new("Kenny", "Yang")
+estelle = Customer.new("Estelle", "Ou")
+Customer.all
+
+kenny.full_name
+estelle.full_name
+
+# Customer.find_by_name("Kenny Yang")
+# Customer.find_by_name("Michael Jackson")
+
+# Customer.find_all_by_first_name("Kenny")
+# Customer.find_all_by_first_name("George")
+
+kenny.add_review(burger_king, 2, "I like burgers")
+
+pho_review = Review.new(kenny, pho, 5, "Pho!!")
+pho_review2 = Review.new(kenny, pho, 10, "Pho!!")
+wendys_review = Review.new(estelle, wendys, 3, "Chickien patties are delicious")
+
+Review.all
+Restaurant.all
+Customer.all
+Customer.all_names
+Restaurant.find_by_name("Burger King")
+Restaurant.find_by_name("Taco Bell")
+
+
+burger_king.reviews
+wendys.reviews
+burger_king.customers
+wendys.customers
+
+pho_review.rating
+
+pho.average_star_rating
+pho.longest_review
+
+kenny.num_reviews
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
