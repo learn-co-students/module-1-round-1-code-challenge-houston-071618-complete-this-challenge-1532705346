d4c7ae5db7b3ac19d86fbbafa0b4a03480f59b32
Scott Tang <fq600@yahoo.com>
2018-07-27 11:31:50 -0500

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review
-  
-end
+  @@all = []
+  attr_reader :customer, :restuarant, :rating
 
+  def initialize(customer, restuarant, rating, review_contents)
+    @customer = customer
+    @restuarant = restuarant
+    @rating = rating
+    @review_contents = review_contents
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  #   def customer
+  #     all.find do |review|
+  #       review.customer == self.customer
+  #     end
+  #   end
+end
