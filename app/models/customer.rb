1d2c57e6698317eb8390df4d1f00c939b7ed0405
github username <github email address>
2018-07-27 11:20:57 -0500

    Done.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,46 @@
 class Customer
   attr_accessor :first_name, :last_name
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |customer| customer.full_name == name }
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select { |customer| customer.first_name == name }
+  end
+
+  def self.all_names
+    @@all.map { |name| name.full_name }
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, rating, content)
+  end
+
+  def num_reviews
+    reviews.length
+  end
+
+  def reviews
+    Review.all.select { |review| review.customer == self }
+  end
+
+  def restaurants
+    reviews.map { |review| review.restaurant }.uniq
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#customers
+first_customer = Customer.new("first", "last")
+second_customer = Customer.new("second", "last")
+third_customer = Customer.new("third", "not last")
+fourth_customer = Customer.new("fourth", "not last")
+fake_first = Customer.new("first", "fake")
+
+#restaurants
+first_restaurant = Restaurant.new("The Very First Restaurant")
+second_restaurant = Restaurant.new("The Very Best Restaurant")
+third_restaurant = Restaurant.new("This Third Rate Restaurant")
+
+#reviews
+first_rev = Review.new(first_customer, first_restaurant, 5, "this is content")
+second_rev = Review.new(second_customer, second_restaurant, 2, "this is also content")
+third_rev = Review.new(third_customer, third_restaurant, 3, "yes, this is content")
+fourth_rev = Review.new(fourth_customer, second_restaurant, 7, "insert content")
+fifth_rev = Review.new(fourth_customer, second_restaurant, 7, "#content")
+sixth_rev = Review.new(second_customer, first_restaurant, 7, "the very first")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
