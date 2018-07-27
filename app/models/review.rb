6ee7c2bdbab5996a62cf5f0c11d2eb0505b1d51d
Kelly <devkells00@gmail.com>
2018-07-27 11:33:13 -0500

    Done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,22 @@
 class Review
+  attr_reader :customer, :restaurant
+  attr_accessor :rating, :content
+
+  @@all = []
+
+  def initialize(customer, restaurant, rating, content)
+    @customer = customer
+    @restaurant = restaurant
+    @rating = rating
+    @content = content
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
   
 end
 
--- tools/console.rb
+++ tools/console.rb
@@ -1,4 +1,7 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/customer.rb'
+require_relative '../app/models/review.rb'
+require_relative '../app/models/restaurant.rb'
 
 def reload
   load 'config/environment.rb'
@@ -6,6 +9,19 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+
+kelly = Customer.new("Kelly", "Lwakatare")
+emil = Customer.new("Emil", "Mutalemwa")
+suzanne = Customer.new("Suzanne", "Johnson")
+
+berryhill = Restaurant.new("Berryhill")
+chipotle = Restaurant.new("Chipotle")
+guadalajara = Restaurant.new("Guadalajara")
+
+review1 = Review.new(kelly, berryhill, 5, "awesome")
+review2 = Review.new(kelly, guadalajara, 4, "decent")
+review3 = Review.new(suzanne, chipotle, 3, "edible")
+review4 = Review.new(emil, berryhill, 4, "great")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
