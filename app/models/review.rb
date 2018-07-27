77770982a59102aa967d59bca1b5fa8441087af0
Sean Huang <hyprul@ucla.edu>
2018-07-27 10:57:46 -0500

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,27 @@
-class Review
-  
+class Review	
+	attr_accessor :rating, :content
+	attr_reader :customer, :restaurant
+
+	@@all = []
+
+
+	def initialize(customer, restaurant, content, rating)
+		@customer = customer
+		@restaurant = restaurant
+		@content = content
+		@rating = rating
+		@@all << self
+	end
+
+	def self.all 
+		@@all 
+	end
+
+  	# def customer 
+  	# end
+
+  	# def restaurant
+  	# end
+
 end
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,35 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
-binding.pry
-0 #leave this here to ensure binding.pry isn't the last line
+
+sean = Customer.new("Sean", "Huang")
+puts sean.full_name
+
+alex = Customer.new("Alex", "Hodor")
+
+
+puts Customer.find_by_name("Sean Huang")
+
+puts Customer.find_all_by_first_name("Sean")
+
+
+johns = Restaurant.new("Papa Johns")
+steaks = Restaurant.new("Steak House")
+
+puts Restaurant.find_by_name("Papa Johns")
+
+best = sean.add_review(steaks, "Best of all Time", 5)
+worst = alex.add_review(johns, "Worst of all Time", 1)
+
+puts sean.num_reviews
+puts sean.restaurants
+
+puts steaks.customers
+puts johns.reviews
+
+puts steaks.average_star_rating
+puts johns.longest_review
+
+
+# binding.pry
+# 0 #leave this here to ensure binding.pry isn't the last line
