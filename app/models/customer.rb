37e7699e0ff4482f287e9b5a376313ded03fce34
RebGov <becci.govert@gmail.com>
2018-07-31 09:04:47 -0500

    looked at notes from Graham and made changes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -20,45 +20,49 @@ class Customer
   end
 
   def self.find_by_name(full_name)
-    
+      list_names = Customer.all.find do |customer|
+          customer.full_name == full_name
+      end
   end
     
   def self.find_all_by_first_name(name)
-    list_names = Customer.all.find_all do |customer|
-      customer.first_name.include?(name)
-    end 
-      list_names.map do |customer|
-        "#{customer.first_name} #{customer.last_name}"
+    list_names = Customer.all.select do |customer|
+        customer.first_name == name
+      end 
+      list_names.map do |name|
+        name.full_name
       end
   end
 
   def self.all_names
     @@all.map do |customer|
-      "#{customer.first_name} #{customer.last_name}"
+      customer.full_name
     end
   end
     
   def add_review(restaurant, content, rating)
      Review.new(self, restaurant, content, rating)
   end
-  
+  def review 
+    Review.all.select do |review|
+      review.customer == self
+    end 
+  end
   def num_reviews 
     #count number of reviews for customer_name
-    customer_reviews = Review.all.select do |review|
-      review.customer == self 
-    end 
-    customer_reviews.count
+    review.count
   end
 
   def restaurants 
     #return a unique array of all restaurants a customer has reviewed. 
-    restaurants_reviewed = Review.all.select do |review|
-      review.customer == self
-    end
-    my_reviewed_restaurants = restaurants_reviewed.map do |review|
-      review.restaurant.name
-    end
-    my_reviewed_restaurants.uniq
+    # names_only_review = review.map do |review|
+      #   review.restaurant.name
+      # end
+      #names_only_review.uniq
+    ##it did not specify if want an instance array or just the names of the restaurants in README
+    ## - `Customer#restaurants`
+    ## - Returns a **unique** array of all restaurants a customer has reviewed
+    review.uniq
   end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,8 @@
 require 'pry'
 
 class Restaurant
-  attr_accessor :name, :review, :customer
+  attr_accessor :name
+  #took off attr_accessor :review, :customer - not needed because review has them.
 
   @@all = []
 
@@ -18,13 +19,8 @@ class Restaurant
     list_restaurants = @@all.find do |restaurant|
       restaurant.name == name
     end 
-    #returns instance of first - but wasn't sure if needed to return in string format
-    # list_restaurants.map do |restaurant|
-    #   restaurant.name
-    # .map do |restaurant|
-    #   "#{restaurant.name}"
-    # end - this was not working so took it out
   end
+
   def customers 
     #returns a unique list of all customers who have reivewed a particular restaurant 
     restaurant_customers = Review.all.select do |review|
@@ -36,22 +32,31 @@ class Restaurant
     restaurant_customer_uniq.uniq
   end
 
+  def review 
+    restaurant_review = Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
 # *****these not done before meeting with Graham****
   def average_star_rating 
+      ratings = self.review.map do |review|
+        review.rating
+      end
+      total_ratings = ratings.inject do |sum, rating|
+        sum + rating
+      end
+      total_ratings.to_f / ratings.size
     #returns the average star rating for a restaurant based on its reviews 
   end 
 
   def longest_review 
     #return the longest review content for a given restaurant
-    #build helper reviews 
-    #
-    # content_count = 0 
-
-    # Review.all.find do |review|
-
-    #   if review.content.length > content_count 
-
-  
+     Review.all.reduce do |longest_review, current_review|
+    if current_review.content.length > longest_review.content.length
+        longest_review = current_review 
+     end 
+     longest_review 
+    end
   end
 
 end
